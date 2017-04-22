b6958b8c755a7a1bc9dcd6280820309e68a91aa0
Stanley Chong <Stanley_chong@outlook.com>
2017-04-22 11:42:42 +0200

    Add files via upload

---
+++ Opdracht 7/Rekentoets.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Rekentoets.aspx.cs" Inherits="Opdracht_7_Stanley_Chong.Rekentoets" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-bottom: 0px;
+        }
+    </style>
+</head>
+<body>
+    <h1 style="font-family: Arial;">Tafeltester</h1>
+
+    <form id="form1" runat="server">
+
+    <p style="font-family: Arial;">Vul het hoogste getal in dat gebruik mag worden in de sommen. 
+        <asp:TextBox ID="txtCijfer" runat="server"></asp:TextBox>
+        </p>
+    <div>
+    
+        <asp:Button ID="btnSommen" runat="server" Text="Maak sommen" OnClick="btnSommen_Click" />
+    
+    </div>
+        <p style="font-family: Arial;">Sommen: </p>
+        <asp:Label ID="lblSom1" runat="server">1 x </asp:Label>
+        <asp:TextBox ID="txtSom1" runat="server" CssClass="auto-style1"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblCorrectie1" runat="server"></asp:Label>
+        <p>
+            <asp:Label ID="lblSom2" runat="server">2 x </asp:Label>
+
+            <asp:TextBox ID="txtSom2" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblCorrectie2" runat="server"></asp:Label>
+        </p>
+        <p>
+
+            <asp:Label ID="lblSom3" runat="server">3 x </asp:Label>
+
+            <asp:TextBox ID="txtSom3" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblCorrectie3" runat="server"></asp:Label>
+        </p>
+        <p>
+
+            <asp:Label ID="lblSom4" runat="server">4 x </asp:Label>
+      
+            <asp:TextBox ID="txtSom4" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblCorrectie4" runat="server"></asp:Label>
+        </p>
+        <p>
+      
+            <asp:Label ID="lblSom5" runat="server">5 x </asp:Label>
+            <asp:TextBox ID="txtSom5" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;
+            <asp:Label ID="lblCorrectie5" runat="server"></asp:Label>
+        </p>
+        <asp:Button ID="btnScore" runat="server" Text="Laat score zien!" OnClick="btnScore_Click" />
+        <p>
+            <asp:Label ID="lblCijfer" runat="server"></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Opdracht 7/Rekentoets.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Opdracht_7_Stanley_Chong
+{
+    public partial class Rekentoets : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSommen_Click(object sender, EventArgs e)
+        {
+            Random RanNummer = new Random();
+
+            int maxNr = Convert.ToInt32(txtCijfer.Text);
+            int Getal1 = RanNummer.Next(0, maxNr) + 1;
+            int Getal2 = RanNummer.Next(0, maxNr) + 1;
+            int Getal3 = RanNummer.Next(0, maxNr) + 1;
+            int Getal4 = RanNummer.Next(0, maxNr) + 1;
+            int Getal5 = RanNummer.Next(0, maxNr) + 1;
+
+
+            lblSom1.Text = lblSom1.Text + Convert.ToString(Getal1);
+            lblSom2.Text = lblSom2.Text + Convert.ToString(Getal2);
+            lblSom3.Text = lblSom3.Text + Convert.ToString(Getal3);
+            lblSom4.Text = lblSom4.Text + Convert.ToString(Getal4);
+            lblSom5.Text = lblSom5.Text + Convert.ToString(Getal5);
+
+        }
+
+        protected void btnScore_Click(object sender, EventArgs e)
+        {
+            int Cijfer = 10;
+            int Correctie1 = 1 * Convert.ToInt32(lblSom1.Text);
+            int Correctie2 = 2 * Convert.ToInt32(lblSom2.Text);
+            int Correctie3 = 3 * Convert.ToInt32(lblSom3.Text);
+            int Correctie4 = 4 * Convert.ToInt32(lblSom4.Text);
+            int Correctie5 = 5 * Convert.ToInt32(lblSom5.Text);
+
+            if (Correctie1 == Convert.ToInt32(txtSom1))
+            {
+                lblCorrectie1.Text = "Goed";
+            }
+            else
+            {
+                lblCorrectie1.Text = "Fout";
+                Cijfer -=2;
+            }
+
+            if (Correctie2 == Convert.ToInt32(txtSom2))
+            {
+                lblCorrectie2.Text = "Goed";
+            }
+            else
+            {
+                lblCorrectie2.Text = "Fout";
+                Cijfer -= 2;
+            }
+
+            if (Correctie3 == Convert.ToInt32(txtSom3))
+            {
+                lblCorrectie3.Text = "Goed";
+            }
+            else
+            {
+                lblCorrectie3.Text = "Fout";
+                Cijfer -= 2;
+            }
+
+            if (Correctie4 == Convert.ToInt32(txtSom4))
+            {
+                lblCorrectie4.Text = "Goed";
+            }
+            else
+            {
+                lblCorrectie4.Text = "Fout";
+                Cijfer -= 2;
+            }
+
+            if (Correctie5 == Convert.ToInt32(txtSom5))
+            {
+                lblCorrectie5.Text = "Goed";
+            }
+            else
+            {
+                lblCorrectie5.Text = "Fout";
+                Cijfer -= 2;
+            }
+
+            lblCijfer.Text = "Je hebt een" + Cijfer + "gehaald.";
+        }
+    }
+}
